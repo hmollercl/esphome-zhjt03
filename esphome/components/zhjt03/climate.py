c5a79ab445ed31f77777370efb4e07fc448561a8
Hans P. Moller <hmoller@gmail.com>
2026-01-17 15:16:22 -0300

    renaming

--- esphome/components/zhjt03/climate.py
+++ esphome/components/zhjt03/climate.py
@@ -3,16 +3,17 @@ import esphome.config_validation as cv
 from esphome.components import climate, remote_transmitter, sensor
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ["remote_transmitter"]
-AUTO_LOAD = ["climate"]
+CONF_TRANSMITTER_ID = "transmitter_id"
+CONF_SENSOR = "sensor"
 
 zhjt03_ns = cg.esphome_ns.namespace("zhjt03")
 ZHJT03Climate = zhjt03_ns.class_("ZHJT03Climate", climate.Climate, cg.Component)
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+CONFIG_SCHEMA = climate.climate_schema(ZHJT03Climate).extend(
     {
         cv.GenerateID(): cv.declare_id(ZHJT03Climate),
-        cv.Optional("sensor"): cv.use_id(sensor.Sensor),
+        cv.Required(CONF_TRANSMITTER_ID): cv.use_id(remote_transmitter.RemoteTransmitterComponent),
+        cv.Optional(CONF_SENSOR): cv.use_id(sensor.Sensor),
     }
 )
 
@@ -21,6 +22,9 @@ async def to_code(config):
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
 
-    if "sensor" in config:
-        sens = await cg.get_variable(config["sensor"])
+    tx = await cg.get_variable(config[CONF_TRANSMITTER_ID])
+    cg.add(var.set_transmitter(tx))
+
+    if CONF_SENSOR in config:
+        sens = await cg.get_variable(config[CONF_SENSOR])
         cg.add(var.set_temperature_sensor(sens))
--- esphome/components/zhjt03/zhjt03_climate.h
+++ esphome/components/zhjt03/zhjt03_climate.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "esphome/components/climate/climate.h"
 #include "esphome/components/remote_transmitter/remote_transmitter.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace zhjt03 {
@@ -8,13 +9,15 @@ namespace zhjt03 {
 class ZHJT03Climate : public climate::Climate, public Component {
  public:
   void set_temperature_sensor(sensor::Sensor *sensor) { this->sensor_ = sensor; }
+  void set_transmitter(remote_transmitter::RemoteTransmitterComponent *tx) { this->tx_ = tx; }
 
-  void setup() override;
+  void setup() override {}
   void control(const climate::ClimateCall &call) override;
   climate::ClimateTraits traits() override;
 
  protected:
   sensor::Sensor *sensor_{nullptr};
+  remote_transmitter::RemoteTransmitterComponent *tx_{nullptr};
 
   bool power_{false};
   climate::ClimateMode mode_{climate::CLIMATE_MODE_OFF};
@@ -22,13 +25,7 @@ class ZHJT03Climate : public climate::Climate, public Component {
 
   void send_state_frame_();
   void send_power_toggle_();
-
-  void transmit_frame_(uint16_t timer,
-                       uint16_t extra,
-                       uint16_t main_cmd,
-                       uint16_t fan,
-                       uint16_t temp_mode,
-                       uint16_t footer);
+  void transmit_frame_(uint16_t timer, uint16_t extra, uint16_t main_cmd, uint16_t fan, uint16_t temp_mode, uint16_t footer);
 };
 
 }  // namespace zhjt03
